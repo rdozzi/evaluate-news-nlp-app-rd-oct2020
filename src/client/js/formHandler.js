d717d6013f30c4a2492bc3739c784bc9669d653c
Raphael Dozzi <rdozzi84@gmail.com>
2020-10-11 15:25:04 -0700

    feat: Updated formHandler
    Updated formHandler on par with client/index.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,14 +2,18 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    let formText = document.getElementById('url').value;
+    console.log("::: Form Submitted :::");
+    fetch('http://localhost:8080/article', {
+        method: "POST",
+    })
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then((res) => {
+        console.log(res);
+        document.getElementById("#score").innerHTML = `Polarity: ${res.score_tag}`;
+        document.getElementById("#subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+        document.getElementById("#confidence").innerHTML = `Confidence: ${res.confidence}`;
+        document.getElementById("#irony").innerHTML = `Irony: ${res.irony}`;
     })
 }
 
