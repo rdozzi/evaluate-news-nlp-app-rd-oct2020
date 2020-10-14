7fc499adbb766e77f761db4ee48c5361bd589336
Raphael Dozzi <rdozzi84@gmail.com>
2020-10-13 20:32:25 -0700

    feat: Added score interpretation function to formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -11,16 +11,33 @@ function handleSubmit(event) {
         headers: {
             "Content-Type": "text/plain"
         },
-        // body: JSON.stringify({url: data})
+
     })
     .then(res => res.json())
     .then((res) => {
         console.log(res);
-        document.getElementById("score").innerHTML = `Polarity: ${res.score_tag}`;
+        document.getElementById("score").innerHTML = `Polarity: ${scoreConverter(res.score_tag).toUpperCase()}`;
         document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+        document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`
         document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
         document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
     })
+}
+
+function scoreConverter(score_tag) {
+    if(score_tag === "P+"){
+        return "Strongly Positive";
+    } else if (score_tag === "P"){
+        return "Positive";
+    } else if (score_tag === "NEU"){
+        return "Neutral";
+    } else if (score_tag === "N"){
+        return "Negative";
+    } else if (score_tag === "N+"){
+        return "Strongly Negative";
+    } else{
+        return "Without Sentiment";
+    }
 }
 
 export { handleSubmit }
