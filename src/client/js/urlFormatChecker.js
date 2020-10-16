3b9b15fc51f6fe64e7f5c870102f011db5efc16d
Raphael Dozzi <rdozzi84@gmail.com>
2020-10-15 23:18:19 -0700

    feat: Created urlFormatChecker to check url syntax

--- src/client/js/urlFormatChecker.js
+++ src/client/js/urlFormatChecker.js
@@ -2,7 +2,7 @@ function urlFormatCheck(input){
     const checkForHTTP = /^http:\/\/|^https:\/\//i;
     const checkForSpace = /\s/;
 
-    if (checkForHTTP && !checkForSpace){
+    if (checkForHTTP.test(input) && !checkForSpace.test(input)){
         return true;
     } else {
         return false;
