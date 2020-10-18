8110636e8e731c7c599439952fe649d5b0af603b
Raphael Dozzi <rdozzi84@gmail.com>
2020-10-17 20:36:01 -0700

    feat: Added test folder & urlFormatCheck primer

---
+++ __test__/urlFormatCheck.test.js
@@ -0,0 +1,16 @@
+import { urlFormatCheck } from './src/client/js.urlFormatChecker'
+
+describe('Check URL Validity', () => {
+    test('URL should return true with "http://" as prefix', () => {
+        const input1 = 'http://www.udacity.com'
+        expect(urlFormatCheck(input1)).toBe(true);
+    });
+    test('URL should return true with "https://" as prefix', () => {
+        const input2 = 'https://www.udacity.com'
+        expect(urlFormatCheck(input2)).toBe(true);
+    });
+    test('URL should return false with non-"http://" or "https://" prefix', () =>{
+        const input3 = 'login.udacity.com'
+        expect (urlFormatCheck(input3)).toBe(false);
+    });
+})
