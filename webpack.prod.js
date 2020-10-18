b22da8c07ea06d94a9b59f1dfcb10e287dc9aba8
Raphael Dozzi <rdozzi84@gmail.com>
2020-10-17 22:50:33 -0700

    feat: Implemented Service Workers Cache feature
    Implemented Service Worker feature per developer.google
    instructions

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -40,5 +41,6 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
         new CleanWebpackPlugin(),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
